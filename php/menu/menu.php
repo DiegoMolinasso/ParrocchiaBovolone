94b804a001e3a140f4e1ebcfb1436a86cb0f43f0
DiegoMolinari06 <diego.molinari@salesianiverona.it>
2024-04-17 17:19:10 +0200

    Init

---
+++ php/menu/menu.php
@@ -0,0 +1,92 @@
+
+        <nav class="sidebar close">
+            <header>
+                <div class="image-text">
+                    <span class="image">
+                        <img src="http://127.0.0.1/progetti/SitoWebParrocchia/images/logo.png" alt="">
+                    </span>
+                    <div class="text logo-text">
+                        <span class="profession">Parrocchia</span>
+                        <span class="name">San Giuseppe</span>
+                    </div>
+                </div>
+                <i class="toggle fa-solid fa-angle-right"></i>
+                
+            </header>
+            <div class="menu-bar">
+                <div class="menu">
+                    <ul class="menu-links">
+                        <li class="nav-link">
+                            <a href="http://127.0.0.1/progetti/SitoWebParrocchia/">
+                                <i class="icon fa-solid fa-house"></i>
+                                <span class="text nav-text">Home</span>
+                            </a>
+                        </li>
+                        <li class="search-box">
+                            <i class="icon fa-solid fa-magnifying-glass"></i>
+                            <input type="text" placeholder="Cerca">
+                        </li>
+                        <li class="nav-link">
+                            <a href="http://127.0.0.1/progetti/SitoWebParrocchia/notizie/notizie.php">
+                                <i class="icon fa-solid fa-newspaper"></i>
+                                <span class="text nav-text">Notizie</span>
+                            </a>
+                        </li>
+                        <li class="nav-link">
+                            <a href="http://127.0.0.1/progetti/SitoWebParrocchia/attivita/attivita.php">
+                                <i class="icon fa-solid fa-signal"></i>
+                                <span class="text nav-text">Attività</span>
+                            </a>
+                        </li>
+                        <li class="nav-link">
+                            <a href="#">
+                            <i class="icon fa-solid fa-envelope-open-text"></i>
+                                <span class="text nav-text">Iscrizioni</span>
+                            </a>
+                        </li>
+                        <li class="nav-link">
+                            <a href="#">
+                            <i class="icon fa-solid fa-video"></i>
+                                <span class="text nav-text">Streaming</span>
+                            </a>
+                        </li>
+                        <li class="nav-link">
+                            <a href="#">
+                                <i class="icon fa-solid fa-clock"></i>
+                                <span class="text nav-text">Orari</span>
+                            </a>
+                        </li>
+                        <li class="nav-link">
+                            <a href="#">
+                                <i class="icon fa-solid fa-phone"></i>
+                                <span class="text nav-text">Contatti</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="bottom-content">
+                    <li class="">
+                        <a href="#">
+                            <i class="icon fa-solid fa-user"></i>
+                            <span class="text nav-text">Account</span>
+                        </a>
+                    </li>
+                    <li class="">
+                        <a href="#">
+                            <i class="icon fa-solid fa-gear"></i>
+                            <span class="text nav-text">Impostazioni</span>
+                        </a>
+                    </li>
+                    <li class="mode">
+                        <div class="sun-moon">
+                            <i class='bx bx-moon icon moon'></i>
+                            <i class='bx bx-sun icon sun'></i>
+                        </div>
+                        <span class="mode-text text">Dark mode</span>
+                        <div class="toggle-switch">
+                            <span class="switch"></span>
+                        </div>
+                    </li>
+                </div>
+            </div>
+        </nav>
---
+++ php/notizie/notizie.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Parrocchia San Giuseppe</title>
+        <link rel='stylesheet' href='menu.css'>
+        <script src="https://kit.fontawesome.com/ed43a70e6b.js" crossorigin="anonymous"></script>
+        <style><?php require '../menu/menu.css' ?></style>
+        
+    </head>
+    <body>
+        <?php require '../menu/menu.php' ?>
+        <script><?php require '../menu/menu.js' ?></script>
+    </body>
+</html>
