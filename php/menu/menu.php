702162b34c140397a9f9dc392b47d19c2d8a64c0
DiegoMolinari06 <diego.molinari@salesianiverona.it>
2024-04-17 18:42:24 +0200

    streaming

--- php/menu/menu.php
+++ php/menu/menu.php
@@ -1,91 +1,90 @@
-
-        <nav class="sidebar close">
-            <header>
-                <div class="image-text">
-                    <span class="image">
-                        <img src="http://127.0.0.1/progetti/ParrocchiaBovolone/images/logo.png" alt="">
-                    </span>
-                    <div class="text logo-text">
-                        <span class="profession">Parrocchia</span>
-                        <span class="name">San Giuseppe</span>
-                    </div>
-                </div>
-                <i class="toggle fa-solid fa-angle-right"></i>
-            </header>
-            <div class="menu-bar">
-                <div class="menu">
-                    <ul class="menu-links">
-                        <li class="nav-link">
-                            <a href="http://127.0.0.1/progetti/ParrocchiaBovolone">
-                                <i class="icon fa-solid fa-house"></i>
-                                <span class="text nav-text">Home</span>
-                            </a>
-                        </li>
-                        <li class="search-box">
-                            <i class="icon fa-solid fa-magnifying-glass"></i>
-                            <input type="text" placeholder="Cerca">
-                        </li>
-                        <li class="nav-link">
-                            <a href="http://127.0.0.1/progetti/ParrocchiaBovolone/php/notizie/notizie.php">
-                                <i class="icon fa-solid fa-newspaper"></i>
-                                <span class="text nav-text">Notizie</span>
-                            </a>
-                        </li>
-                        <li class="nav-link">
-                            <a href="http://127.0.0.1/progetti/ParrocchiaBovolone/php/attivita/attivita.php">
-                                <i class="icon fa-solid fa-signal"></i>
-                                <span class="text nav-text">Attività</span>
-                            </a>
-                        </li>
-                        <li class="nav-link">
-                            <a href="http://127.0.0.1/progetti/ParrocchiaBovolone/php/iscrizioni/iscrizioni.php">
-                            <i class="icon fa-solid fa-envelope-open-text"></i>
-                                <span class="text nav-text">Iscrizioni</span>
-                            </a>
-                        </li>
-                        <li class="nav-link">
-                            <a href="http://127.0.0.1/progetti/ParrocchiaBovolone/php/streaming/streaming.php">
-                            <i class="icon fa-solid fa-video"></i>
-                                <span class="text nav-text">Streaming</span>
-                            </a>
-                        </li>
-                        <li class="nav-link">
-                            <a href="http://127.0.0.1/progetti/ParrocchiaBovolone/php/orari/orari.php">
-                                <i class="icon fa-solid fa-clock"></i>
-                                <span class="text nav-text">Orari</span>
-                            </a>
-                        </li>
-                        <li class="nav-link">
-                            <a href="http://127.0.0.1/progetti/ParrocchiaBovolone/php/contatti/contatti.php">
-                                <i class="icon fa-solid fa-phone"></i>
-                                <span class="text nav-text">Contatti</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="bottom-content">
-                    <li class="">
-                        <a href="#">
-                            <i class="icon fa-solid fa-user"></i>
-                            <span class="text nav-text">Account</span>
-                        </a>
-                    </li>
-                    <li class="">
-                        <a href="#">
-                            <i class="icon fa-solid fa-gear"></i>
-                            <span class="text nav-text">Impostazioni</span>
-                        </a>
-                    </li>
-                    <li class="mode">
-                        <div class="sun-moon">
-                            <i class='bx bx-moon icon moon'></i>
-                            <i class='bx bx-sun icon sun'></i>
-                        </div>
-                        <span class="mode-text text">Dark mode</span>
-                        <div class="toggle-switch">
-                            <span class="switch"></span>
-                        </div>
-                    </li>
-                </div>
+<nav class="sidebar close">
+    <header>
+        <div class="image-text">
+            <span class="image">
+                <img src="http://127.0.0.1/progetti/ParrocchiaBovolone/images/logo.png" alt="">
+            </span>
+            <div class="text logo-text">
+                <span class="profession">Parrocchia</span>
+                <span class="name">San Giuseppe</span>
             </div>
-        </nav>
+        </div>
+        <i class="toggle fa-solid fa-angle-right"></i>
+    </header>
+    <div class="menu-bar">
+        <div class="menu">
+            <ul class="menu-links">
+                <li class="nav-link">
+                    <a href="http://127.0.0.1/progetti/ParrocchiaBovolone">
+                        <i class="icon fa-solid fa-house"></i>
+                        <span class="text nav-text">Home</span>
+                    </a>
+                </li>
+                <li class="search-box">
+                    <i class="icon fa-solid fa-magnifying-glass"></i>
+                    <input type="text" placeholder="Cerca">
+                </li>
+                <li class="nav-link">
+                    <a href="http://127.0.0.1/progetti/ParrocchiaBovolone/php/notizie/notizie.php">
+                        <i class="icon fa-solid fa-newspaper"></i>
+                        <span class="text nav-text">Notizie</span>
+                    </a>
+                </li>
+                <li class="nav-link">
+                    <a href="http://127.0.0.1/progetti/ParrocchiaBovolone/php/attivita/attivita.php">
+                        <i class="icon fa-solid fa-signal"></i>
+                        <span class="text nav-text">Attività</span>
+                    </a>
+                </li>
+                <li class="nav-link">
+                    <a href="http://127.0.0.1/progetti/ParrocchiaBovolone/php/iscrizioni/iscrizioni.php">
+                    <i class="icon fa-solid fa-envelope-open-text"></i>
+                        <span class="text nav-text">Iscrizioni</span>
+                    </a>
+                </li>
+                <li class="nav-link">
+                    <a href="http://127.0.0.1/progetti/ParrocchiaBovolone/php/streaming/streaming.php">
+                    <i class="icon fa-solid fa-video"></i>
+                        <span class="text nav-text">Streaming</span>
+                    </a>
+                </li>
+                <li class="nav-link">
+                    <a href="http://127.0.0.1/progetti/ParrocchiaBovolone/php/orari/orari.php">
+                        <i class="icon fa-solid fa-clock"></i>
+                        <span class="text nav-text">Orari</span>
+                    </a>
+                </li>
+                <li class="nav-link">
+                    <a href="http://127.0.0.1/progetti/ParrocchiaBovolone/php/contatti/contatti.php">
+                        <i class="icon fa-solid fa-phone"></i>
+                        <span class="text nav-text">Contatti</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="bottom-content">
+            <li class="">
+                <a href="#">
+                    <i class="icon fa-solid fa-user"></i>
+                    <span class="text nav-text">Account</span>
+                </a>
+            </li>
+            <li class="">
+                <a href="#">
+                    <i class="icon fa-solid fa-gear"></i>
+                    <span class="text nav-text">Impostazioni</span>
+                </a>
+            </li>
+            <li class="mode">
+                <div class="sun-moon">
+                    <i class='bx bx-moon icon moon'></i>
+                    <i class='bx bx-sun icon sun'></i>
+                </div>
+                <span class="mode-text text">Dark mode</span>
+                <div class="toggle-switch">
+                    <span class="switch"></span>
+                </div>
+            </li>
+        </div>
+    </div>
+</nav>
--- php/streaming/streaming.php
+++ php/streaming/streaming.php
@@ -5,10 +5,18 @@
         <title>Parrocchia San Giuseppe</title>
         <link rel='stylesheet' href='menu.css'>
         <script src="https://kit.fontawesome.com/ed43a70e6b.js" crossorigin="anonymous"></script>
-        <style><?php require '../../css/menu/menu.css' ?></style>
+        <style>
+            <?php require '../../css/menu/menu.css' ?>
+            <?php require '../../css/streaming/streaming.css' ?>
+        </style>
     </head>
     <body>
         <?php require '../../php/menu/menu.php' ?>
-        <script><?php require '../../js/menu/menu.js' ?></script>
+        <section class='home'>
+        <iframe class="video_iframe" src="https://play.xdevel.com/12669" scrolling="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="no"></iframe>
+        </section>
+        <script>
+            <?php require '../../js/menu/menu.js' ?>
+        </script>
     </body>
 </html>
