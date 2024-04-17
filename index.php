aaf54831784a5aa2ca78f895cf9c612f455b5f3f
DiegoMolinari06 <diego.molinari@salesianiverona.it>
2024-04-17 19:58:22 +0200

    css menu

--- index.php
+++ index.php
@@ -5,11 +5,15 @@
         <title>Parrocchia San Giuseppe</title>
         <link rel='stylesheet' href='menu.css'>
         <script src="https://kit.fontawesome.com/ed43a70e6b.js" crossorigin="anonymous"></script>
-        <style><?php require 'css/menu/menu.css' ?></style>
+        <style>
+            <?php require 'css/menu/menu.css' ?>
+        </style>
     </head>
     <body>
         <h1>CIAO</h1>
         <?php require 'php/menu/menu.php' ?>
-        <script><?php require 'js/menu/menu.js' ?></script>
+        <script>
+            <?php require 'js/menu/menu.js' ?>
+        </script>
     </body>
 </html>
